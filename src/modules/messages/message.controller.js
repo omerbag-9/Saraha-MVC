292b048679ca093bfd17958b0a06916b29d4a99e
omerbag-9 <omerbagprog@gmail.com>
2024-09-03 04:02:43 +0300

    production url

--- src/modules/messages/message.controller.js
+++ src/modules/messages/message.controller.js
@@ -2,7 +2,7 @@ import Qrcode from 'qrcode'
 import { Message } from '../../../db/models/message.model.js'
 export const getMessage = async (req,res,next)=>{
     if(req.session.isLoggedIn){
-        let url = `http://localhost:3000/user/${req.session.userId}`
+        let url = `${req.protocol}://${req.headers.host}/user/${req.session.userId}`
         let qrCode = await Qrcode.toDataURL(url) 
         const messages = await Message.find({user:req.session.userId})       
         return res.render('message.ejs', { url, qrCode,messages })
